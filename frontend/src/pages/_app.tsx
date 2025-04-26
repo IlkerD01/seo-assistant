4d279053ea31e1b2212ea5bc035794e5b195e09d
IlkerD01 <195546057+IlkerD01@users.noreply.github.com>
2025-04-26 16:15:29 +0200

    Create _app.tsx

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import Head from 'next/head';
+
+<Head>
+  <title>SEO Assistant - Boost Your SEO</title>
+  <meta name="description" content="Unlock premium SEO tools with SEO Assistant." />
+</Head>
