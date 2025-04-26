a58bdf7b978e33309e7d56b7baca58e4c77385f1
IlkerD01 <195546057+IlkerD01@users.noreply.github.com>
2025-04-26 18:38:00 +0200

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import Head from 'next/head';
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 <Head>
   <title>SEO Assistant - Boost Your SEO</title>
   <meta name="description" content="Unlock premium SEO tools with SEO Assistant." />
 </Head>
+
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
