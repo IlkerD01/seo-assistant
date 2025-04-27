3208e488c3cddb2a326d0fe07e1f7f470f8dd8a2
IlkerD01 <195546057+IlkerD01@users.noreply.github.com>
2025-04-27 22:07:35 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,10 @@
-# FastAPI app include router admin_routes
+from fastapi import FastAPI
+from .stripe_routes import stripe_router
+
+app = FastAPI()
+
+app.include_router(stripe_router)
+
+@app.get("/")
+def read_root():
+    return {"message": "SEO Assistant Backend Running"}# FastAPI app include router admin_routes
