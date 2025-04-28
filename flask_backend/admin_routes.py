f1ee87e594256a66162497ae6bea41922d211c0f
Ilker <idem.85@hotmail.com>
2025-04-28 12:35:47 +0200

    Update admin_routes.py

--- flask_backend/admin_routes.py
+++ flask_backend/admin_routes.py
@@ -85,24 +85,4 @@ def export_searches():
         as_attachment=True,
         download_name=f"zoekopdrachten_export_{datetime.date.today()}.csv"
     )
-@admin_bp.route('/api/admin/export-searches', methods=['GET'])
-def export_searches():
-    output = io.StringIO()
-    writer = csv.writer(output)
-    
-    # CSV Header
-    writer.writerow(['Gebruiker Email', 'Zoekopdracht', 'Tijdstip'])
 
-    users = User.query.all()
-    for user in users:
-        for search in user.searches:
-            writer.writerow([user.email, search.query_text, search.timestamp])
-
-    output.seek(0)
-
-    return send_file(
-        io.BytesIO(output.getvalue().encode()),
-        mimetype='text/csv',
-        as_attachment=True,
-        download_name=f"zoekopdrachten_export_{datetime.date.today()}.csv"
-    )
