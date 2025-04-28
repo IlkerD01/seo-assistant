f7e14cfb875021598daf9a774ba26af094775903
Ilker <idem.85@hotmail.com>
2025-04-28 15:01:15 +0200

    Update app.py

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from models import db
 from admin_routes import admin_bp
+from billing_routes import billing_bp
 
 def create_app():
     app = Flask(__name__)
@@ -17,6 +18,7 @@ def create_app():
 
     # Register blueprints
     app.register_blueprint(admin_bp)
+    app.register_blueprint(billing_bp)
 
     from auth_routes import auth_bp
     app.register_blueprint(auth_bp)
